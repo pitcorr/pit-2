65e751c0ad7ca6c072559fc1ff44d06ca5476f45
pitcorr <pitcorr@gmail.com>
2014-12-04 22:47:05 +0200

    friendly uri

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
 <?php
+
+isset($_GET['page']) ? $page = $_GET['page'] : $page = 'home';
+$page = str_replace('/', '', $page);
+$file = $_SERVER['DOCUMENT_ROOT'].'/view/'.$page.'.phtml';
+if(file_exists($file)){
+    ob_start();
+    include ("$file");
+    $content = ob_get_clean();
+    include('view/layout.phtml');
+} else echo 'Страница не найдена';
